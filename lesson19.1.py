a90427fea6bf68546a4082d36665d0a4ec58b689
Koluniadrag <86798489+Koluniadrag@users.noreply.github.com>
2023-04-28 21:25:10 +0300

    Add files via upload

---
+++ lesson19.1.py
@@ -0,0 +1,9 @@
+def with_index(iterable,start=0):
+    n = start
+    for elem in iterable:
+        yield n, elem
+        n += 1
+
+
+l = ["перший", "другий", "третій"]
+print(list(with_index(l,1)))
